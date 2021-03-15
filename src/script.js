49fb15cf3074ec0d77674795e45d638888d45384
Manal <duraibimn@gmail.com>
2021-03-15 00:29:25 -0500

    update the time

--- src/script.js
+++ src/script.js
@@ -1,29 +1,60 @@
+
+
+
+
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+    let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`
+
+}
+
 function showWeather(response) {
     console.log(url);
 
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML= Math.round(response.data.main.temp);
 
-    let nameCity = document.querySelector("#city");
-    nameCity.innerHTML= response.data.name;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= response.data.name;
 
-    let weatherDescription = document.querySelector("#description");
-    weatherDescription.innerHTML= response.data.weather[0].description;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML= response.data.weather[0].description;
 
     let humidity = response.data.main.humidity;
 
-    let humidityLevel = document.querySelector("#humidity");
-    humidityLevel.innerHTML= `Humidity : ${humidity} %`;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML= `Humidity : ${humidity} %`;
 
     let wind = Math.round(response.data.wind.speed);
 
-    let windSpeed = document.querySelector("#wind");
-    windSpeed.innerHTML= `Wind : ${wind} km`;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML= `Wind : ${wind} km`;
+
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
 let apiKey = "0196dac33373aaa2798921754f07b116";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Jeddah&appid=${apiKey}&units=metric`;
 
 
 // let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
